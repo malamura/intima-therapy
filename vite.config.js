1ee94ddb06224b1ed9c1dc1eb93054c34367a53b
malamura <office@yourfuture.com.ua>
2026-02-23 13:43:25 +0100

    footer upd

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 therapists: resolve(__dirname, 'our-therapists.html'),
-                // додайте сюди інші сторінки, якщо вони є, наприклад:
-                // about: resolve(__dirname, 'about.html'),
+                profile: resolve(__dirname, 'therapist-profile.html'),
             },
         },
     },
