38a7a75be0e6414b2b9ffe8590e4921a0cf0a22d
malamura <office@yourfuture.com.ua>
2026-02-23 11:43:53 +0100

    Our Terapists resposive versel 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                therapists: resolve(__dirname, 'our-therapists.html'),
+                // додайте сюди інші сторінки, якщо вони є, наприклад:
+                // about: resolve(__dirname, 'about.html'),
+            },
+        },
+    },
 });
